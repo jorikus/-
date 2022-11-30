47254e1a5846fa4b8277a280a1ceff0961070f23
jorikus <voroschilow.eg@yandex.ru>
2022-11-30 13:15:12 +0300

    lab3,4

---
+++ lab3(misha)/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+	class Abiturient
+	{
+		public Abiturient(string se, string ne, string oc, string adr, int[] o)
+		{
+			surname = se;
+			name = ne;
+			otec = oc;
+			adress = adr;
+			ocen = o;
+		}
+		string surname;
+		string name;
+		string otec;
+		string adress;
+		int[] ocen;
+
+		string O(int[] array)
+		{
+			string res = "";
+			for (int i = 0; i < array.Length; i++)
+			{
+				res += $"{Convert.ToString(array[i])} ";
+			}
+			return res;
+		}
+		public bool udovl()
+		{
+			int[] massiv = ocen;
+			for (int i = 0; i < massiv.Length; i++)
+			{
+				if (massiv[i] <= 2)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+		public bool summ(int sum)
+		{
+			int[] massiv = ocen;
+			int main_sum = 0;
+			for (int i = 0; i < massiv.Length; i++)
+			{
+				main_sum += massiv[i];
+			}
+
+			if (sum >= main_sum)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public int return_summ()
+		{
+			int[] massiv = ocen;
+			int main_sum = 0;
+			for (int i = 0; i < massiv.Length; i++)
+			{
+				main_sum += massiv[i];
+			}
+			return main_sum;
+		}
+
+		public string print()
+		{
+			return String.Format($"Фамилия: {surname}\nИмя: {name}\nОтчество: {otec}\nАдресс: {adress}\nОценки: {O(ocen)}");
+
+		}
+	}
+	class Program
+	{
+		static Abiturient[] Sort(Abiturient[] array)
+		{
+			int len = array.Length;
+			for (int i = 1; i < len; i++)
+			{
+				for (int j = 0; j < len - i; j++)
+				{
+					if (array[j].return_summ() < array[j + 1].return_summ())
+					{
+						Abiturient temp = array[j + 1];
+						array[i] = array[j];
+						array[j] = temp;
+
+					}
+				}
+			}
+			return array;
+		}
+
+		static void Main(string[] args)
+		{
+			Random rnd = new Random();
+
+			string[] array1 = new string[4] { "Окулов", "Акулов", "Брехин", "Брёхин" };
+			string[] array2 = new string[4] { "Максим", "Олег", "Рюрик", "Умер" };
+			string[] array3 = new string[4] { "Акулович", "Кабанович", "Окулович", "Егорович" };
+			string[] array4 = new string[4] { "Ул. Юности 11", "Ул. Юности 12", "Ул. Юности 13", "Ул. Юности 14" };
+
+			int n = 10;
+			Abiturient[] array = new Abiturient[n];
+
+			for (int i = 0; i < n; i++)
+			{
+				string f = array1[rnd.Next(0, 4)];
+				string i1 = array2[rnd.Next(0, 4)];
+				string otc = array3[rnd.Next(0, 4)];
+				string ad = array4[rnd.Next(0, 4)];
+				int[] mark = new int[4] { rnd.Next(1, 6), rnd.Next(1, 6), rnd.Next(1, 6), rnd.Next(1, 6) };
+				Abiturient a = new Abiturient(f, i1, otc, ad, mark);
+				array[i] = a;
+			}
+			for (int i = 0; i < n; i++)
+			{
+				Console.Write(array[i].print());
+				Console.Write($"\nСумма балов: {array[i].return_summ()}");
+				Console.Write("\n\n");
+			}
+
+			Console.WriteLine("----------------------------------------------");
+			Console.WriteLine("Список абитуриентов, имеющих неудовлетворительные оценки");
+			for (int i = 0; i < n; i++)
+			{
+				if (!array[i].udovl())
+				{
+					Console.Write(array[i].print());
+					Console.Write($"\nСумма балов: {array[i].return_summ()}");
+					Console.Write("\n\n");
+				}
+			}
+
+			Console.WriteLine("----------------------------------------------");
+			Console.Write("Введите сумму баллов: ");
+			int sum = int.Parse(Console.ReadLine());
+			Console.WriteLine("Список абитуриентов, сумма баллов у которых не меньше заданной");
+			for (int i = 0; i < n; i++)
+			{
+				if (!array[i].summ(sum))
+				{
+					Console.Write(array[i].print());
+					Console.Write($"\nСумма балов: {array[i].return_summ()}");
+					Console.Write("\n\n");
+				}
+			}
+
+			Console.WriteLine("----------------------------------------------");
+			Console.Write("Введите кол-во: ");
+			int N = int.Parse(Console.ReadLine());
+			array = Sort(array);
+			int pp_sum = 0;
+			for (int i = 0; i < N; i++)
+            {
+				Console.Write(array[i].print());
+				Console.Write($"\nСумма балов: {array[i].return_summ()}");
+				Console.Write("\n\n");
+				pp_sum = array[i].return_summ();
+			}
+
+			Console.WriteLine("----------------------------------------------");
+			Console.WriteLine("Cписок абитуриентов, имеющих полупроходной балл");
+			int k = 0;
+			for (int i = 0; i < n; i++)
+            {
+				if (array[i].return_summ() == pp_sum)
+                {
+					k++;
+				}
+            }
+
+			if (k > 1)
+            {
+				for (int i = 0; i < n; i++)
+				{
+					if (array[i].return_summ() == pp_sum)
+                    {
+						Console.Write(array[i].print());
+						Console.Write($"\nСумма балов: {array[i].return_summ()}");
+						Console.Write("\n\n");
+					}
+				}
+			}
+		}
+	}
+}
---
+++ lab4(misha)/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			mode = DrowMode.LINE;
+		}
+
+		enum DrowMode
+		{
+			PEN,
+			LINE,
+			ELLIPS,
+			RECTANGLE
+		}
+
+		Point startPoint;
+		Line line;
+		Ellipse ellipse;
+		Rectangle rectangle;
+
+		Color color;
+		Color fillColor;
+
+		DrowMode mode;
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			mode = DrowMode.LINE;
+		}
+		private void Button_Click_1(object sender, RoutedEventArgs e)
+		{
+			mode = DrowMode.PEN;
+		}
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+			mode = DrowMode.ELLIPS;
+		}
+		private void Button_Click_3(object sender, RoutedEventArgs e)
+		{
+			mode = DrowMode.RECTANGLE;
+		}
+
+		private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+			startPoint = e.GetPosition(canvas);
+            switch (mode)
+            {
+				case DrowMode.PEN:
+					startPoint = e.GetPosition(canvas);
+					break;
+				case DrowMode.LINE:
+					line = new Line();
+					line.Stroke = new SolidColorBrush(color);
+					line.X1 = startPoint.X;
+					line.X2 = startPoint.X;
+					line.Y1 = startPoint.Y;
+					line.Y2 = startPoint.Y;
+					canvas.Children.Add(line);
+					break;
+				case DrowMode.ELLIPS:
+					ellipse = new Ellipse();
+					ellipse.Stroke = new SolidColorBrush(color);
+					canvas.Children.Add(ellipse);
+					Canvas.SetTop(ellipse, startPoint.Y);
+					Canvas.SetLeft(ellipse, startPoint.X);
+					ellipse.Width = 0;
+					ellipse.Height = 0;
+					break;
+				case DrowMode.RECTANGLE:
+					rectangle = new Rectangle();
+					rectangle.Stroke = new SolidColorBrush(color);
+					canvas.Children.Add(rectangle);
+					Canvas.SetTop(rectangle, startPoint.Y);
+					Canvas.SetLeft(rectangle, startPoint.X);
+					rectangle.Width = 0;
+					rectangle.Height = 0;
+					break;
+			}
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+			if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                switch (mode)
+                {
+					case DrowMode.PEN:
+						Point tempPoint_1 = e.GetPosition(canvas);
+						line = new Line();
+						line.Stroke = new SolidColorBrush(color);
+						line.X1 = startPoint.X;
+						line.Y1 = startPoint.Y;
+						line.X2 = tempPoint_1.X;
+						line.Y2 = tempPoint_1.Y;
+						canvas.Children.Add(line);
+						startPoint = tempPoint_1;
+						break;
+					case DrowMode.LINE:
+						line.X2 = e.GetPosition(canvas).X;
+						line.Y2 = e.GetPosition(canvas).Y;
+						break;
+					case DrowMode.ELLIPS:
+						Point tempPoint_2 = e.GetPosition(canvas);
+						double w = tempPoint_2.X - startPoint.X;
+						double h = tempPoint_2.Y - startPoint.Y;
+						if (w < 0)
+                        {
+							w = -w;
+							Canvas.SetLeft(ellipse, tempPoint_2.X);
+                        }
+						if (h < 0)
+                        {
+							h = -h;
+							Canvas.SetTop(ellipse, tempPoint_2.Y);
+                        }
+						ellipse.Width = w;
+						ellipse.Height = h;
+						break;
+					case DrowMode.RECTANGLE:
+						Point tempPoint_3 = e.GetPosition(canvas);
+						double w2 = tempPoint_3.X - startPoint.X;
+						double h2 = tempPoint_3.Y - startPoint.Y;
+						if (w2 < 0)
+						{
+							w2 = -w2;
+							Canvas.SetLeft(rectangle, tempPoint_3.X);
+						}
+						if (h2 < 0)
+						{
+							h2 = -h2;
+							Canvas.SetTop(rectangle, tempPoint_3.Y);
+						}
+						rectangle.Width = w2;
+						rectangle.Height = h2;
+						break;
+				}
+            }
+        }
+
+        private void Label_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+			if (e.LeftButton == MouseButtonState.Pressed)
+			{
+				color = ((SolidColorBrush)((Label)sender).Background).Color;
+			}
+			else if (e.RightButton == MouseButtonState.Pressed)
+			{
+				fillColor = ((SolidColorBrush)((Label)sender).Background).Color;
+			}
+		}
+
+        
+    }
+}
